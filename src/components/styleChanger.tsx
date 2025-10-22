02aef2d0c7cc0f6021f132f5365b4b62805d14ac
Victor Cavalcanti <victor.mca.dev@gmail.com>
2025-10-21 23:41:08 -0300

    init: Repository initialized with basic libs

---
+++ src/components/styleChanger.tsx
@@ -0,0 +1,13 @@
+export function StyleChanger(props: {asset: string}): React.ReactElement {
+  const asset = props.asset;
+  function setStyle() {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = `./styles/${asset}`
+    document.head.appendChild(link);
+  }
+
+  return(
+    <button onClick={setStyle}>Set {asset} as style</button>
+  )
+}
